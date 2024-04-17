b0ce76f8d11e42ae6488a44dab5e1caba9788020
Carlo Casorzo <carlo@deepblu.dev>
2024-04-17 19:42:50 +0200

    chore: extract examples and rename folders

---
+++ examples/src/main.rs
@@ -0,0 +1,29 @@
+use std::time::{Duration, Instant};
+
+use routrs::Geolocalizable;
+use routrs::MARITIME;
+
+fn main() {
+    let mut iter = 0;
+    let mut total_time = Duration::new(0, 0);
+
+    let first_node = MARITIME.get(1).unwrap();
+    for node in MARITIME.nodes() {
+        iter += 1;
+        let start = Instant::now(); // Start timing
+        let (distance, path, path_type) = MARITIME.distance(first_node, node).unwrap();
+        let duration = start.elapsed();
+        total_time += duration;
+        let avg_time = total_time / iter;
+
+        println!(
+            "{:?}\t{:?}\t{:?}\t\tdistance: {:?} km ({:?} nodes) {:?}",
+            avg_time,
+            first_node.geoloc(),
+            node.geoloc(),
+            distance,
+            path.len(),
+            path_type
+        );
+    }
+}
